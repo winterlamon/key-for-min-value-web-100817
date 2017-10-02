8d8b485291976569a4207935b979f536fdd40f03
Winter LaMon <winter.lamon@gmail.com>
2017-10-02 15:22:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
-  if name_hash == {}
-    return nil
+#  if name_hash == {}
+#    return nil
   else name_hash.each do |key, value|
-    if value < min_value
+    if min_value == nil || value < min_value
       min_value = value
       min_key = key
       end
